0001f7491a162239df658ef9e16b0a358b814491
Qonus <94901082+Qonus@users.noreply.github.com>
2024-08-24 17:13:09 +0500

    [Add] Sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+import path from "path";
+
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(process.cwd(), "styles")],
+    prependData: `@import "main.scss";`,
+  },
+};
 
 export default nextConfig;
