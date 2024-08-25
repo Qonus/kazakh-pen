42cb13cb490fb383bda703a1932e6a30790123ca
Boiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <bolathanzada@gmail.com>
2024-08-25 16:30:31 +0500

    [Add] homepage template

--- components/Homepage/Homepage.tsx
+++ components/Homepage/Homepage.tsx
@@ -4,33 +4,41 @@ import Header from "@/components/Header/Header";
 import Button from "@/components/Button/Button";
 import Standart_text from "@/components/Standart_text/standart_text";
 import styles from "./Homepage.module.scss";
-
-function Redirect(pathname: string) {
-  location.href = `${pathname}test`;
-}
+import Link from "next/link";
 
 export default function Homepage() {
   const pathname = usePathname();
   return (
-    <div className={styles.main_section}>
-      <div className={styles.main_section_wrapper}>
-        <div className={styles.main_section__content_left}>
-          <Header text="Халуми" />
-          <Standart_text text="это стандартный текст про халуми" />
-        </div>
-        <div className={styles.main_section__content_right}>
-          <Button
-            text="Поесть халмуми"
-            header="Халуми это гуд"
-            сlick={() => Redirect(pathname)}
-          />
-          <Button
-            text="Поесть халмуми"
-            header="Халуми это гуд"
-            сlick={() => Redirect(pathname)}
-          />
+    <div>
+      <div className={styles.main_section}>
+        <div className={styles.main_section_wrapper}>
+          <div className={styles.main_section__content_left}>
+            <Header text="Халуми" />
+            <Standart_text text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget ligula ante. Morbi placerat sapien eget diam porta consectetur. Donec vitae felis et odio feugiat vehicula. Pellentesque consectetur ligula eget libero bibendum imperdiet eu id erat. Aenean semper est ipsum, sed venenatis velit sodales sed. Nunc mattis, erat sit amet vulputate tristique, augue est mollis mauris, in cursus nibh purus eget nunc. Sed et laoreet lacus, quis rhoncus mi. " />
+          </div>
+          <div className={styles.main_section__content_right}>
+            <Link href ="/test" >
+            <Button
+              text="Поесть халуми"
+             header="Халуми это гуд"
+            /> </Link>
+            
+            <Link href ="/test" >
+            <Button
+              text="Поесть халуми"
+             header="Халуми это гуд"
+            /> </Link>
+          </div>
         </div>
       </div>
+      
+      <div className={styles.secondary_section}>
+        <div className={styles.secondary_section_wrapper}>
+        <Standart_text text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi eget ligula ante. Morbi placerat sapien eget diam porta consectetur. Donec vitae felis et odio feugiat vehicula. Pellentesque consectetur ligula eget libero bibendum imperdiet eu id erat. Aenean semper est ipsum, sed venenatis velit sodales sed. Nunc mattis, erat sit amet vulputate tristique, augue est mollis mauris, in cursus nibh purus eget nunc. Sed et laoreet lacus, quis rhoncus mi. " />
+        </div>
+      </div>
+      
     </div>
+    
   );
 }
