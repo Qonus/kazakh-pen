0127a0819aeef07d33d85a597ce739808620bae0
Boiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <bolathanzada@gmail.com>
2024-08-25 22:00:06 +0500

    [Add] author page 1

--- app/(search)/authors/[id]/page.tsx
+++ app/(search)/authors/[id]/page.tsx
@@ -1,5 +1,25 @@
-import styles from "page.module.scss";
+import Image from "next/image";
+import styles from "./page.module.scss";
 
 export default function AuthorPage({ params }: any) {
-  return <></>;
+  return (
+    <div className={styles.author_page}> 
+      <div className={styles.author_page_wrapper}>
+        <div className={styles.author_page__info}>
+          <div>
+          <Image
+          src="/Abai_Kunanbaev.jpg"
+          alt="Picture of the author"
+          width={150}
+          height={150}
+          className={styles.user_img} />  
+          </div>
+          <div>
+            <h1>Абай Құнанбаев</h1>
+            <p>Гигатекст для гигаопльзователя</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    );
 }
