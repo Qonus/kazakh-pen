823efaa783e13798da522d4691bf454c4220c1ed
Boiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <bolathanzada@gmail.com>
2024-08-26 21:01:43 +0500

    [Add] ellipsis

--- components/AuthorInfo/AuthorInfo.tsx
+++ components/AuthorInfo/AuthorInfo.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import styles from "./AuthorInfo.module.scss"; 
 
-export default function AuthorInfo({params = {firstName: "Abai", lastName:"Kunanbaev", birthDate:"1999", deathDate:"2000", description:""}}:
-    {params?:{firstName: string, lastName: string, birthDate: string, deathDate: string, description: string}}){
+export default function AuthorInfo({params = {firstName: "Abai", lastName:"Kunanbaev", birthDate:"1999", deathDate:"2000", description:"",  likes: "", relatedArticles: ""}}:
+    {params?:{firstName: string, lastName: string, birthDate: string, deathDate: string, description: string, likes: string, relatedArticles:string}}){
     return(
     <div className={styles.info + " glass"}>
         <div className={styles.info__left}>
@@ -12,12 +12,12 @@ export default function AuthorInfo({params = {firstName: "Abai", lastName:"Kunan
             width={150}
             height={150}
             className={styles.user_img} />
-            <p className={styles.dates}>{params.birthDate}-{params.deathDate}</p>
+            <p className={styles.secondary_text}>({params.birthDate}-{params.deathDate})</p>
         </div>
         <div className={styles.info__right}>
             <h1>{params.firstName}_{params.lastName}</h1>
-            
-            <p>{params.description}</p>
+            <p className={styles.secondary_text}> ❤️{params.likes}_Связанных статей: {params.relatedArticles}</p>
+            <p className={styles.ellipsis}>{params.description}</p>
         </div>
     </div>
     )
