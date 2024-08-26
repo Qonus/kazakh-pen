2b4d7d0238a9fa9b133f8b0b4db4076f2b3e51b5
Qonus <94901082+Qonus@users.noreply.github.com>
2024-08-26 21:54:36 +0500

    [Update] AuthorInfo, ArticleCard styles

--- components/AuthorInfo/AuthorInfo.tsx
+++ components/AuthorInfo/AuthorInfo.tsx
@@ -6,34 +6,43 @@ export default function AuthorInfo({
   lastName = "Kunanbaev",
   birthDate = "1999",
   deathDate = "2000",
+  nationality = "Казак",
   description = "",
 }: {
   firstName: string;
   lastName: string;
   birthDate: string;
   deathDate: string;
+  nationality: string;
   description: string;
 }) {
   return (
-    <div className={styles.info + " glass"}>
-      <div className={styles.info__left}>
+    <div className={styles.author_info + " glass"}>
+      <div className={styles.author_info__left}>
         <Image
+          className={styles.author_info__left__image}
           src="/Abai_Kunanbaev.jpg"
           alt="Picture of the author"
           width={200}
           height={200}
-          className={styles.user_img}
         />
-        <p className={styles.dates}>
+        <p className={styles.author_info__text}>
           {birthDate} - {deathDate}
         </p>
+        <p className={styles.author_info__text}>{nationality}</p>
       </div>
-      <div className={styles.info__right}>
+      <div className={styles.author_info__right}>
         <h1>
           {firstName} {lastName}
         </h1>
 
-        <p>{description}</p>
+        <p
+          className={
+            styles.author_info__text + " " + styles.author_info__ellipsis
+          }
+        >
+          {description}
+        </p>
       </div>
     </div>
   );
