0cf98d77d0425c0e3115b5368154dd550b16ccc9
Boiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <bolathanzada@gmail.com>
2024-08-26 17:52:53 +0500

    [Change] author page stylistic changes

--- app/(search)/articles/[id]/page.tsx
+++ app/(search)/articles/[id]/page.tsx
@@ -1,11 +1,11 @@
 import styles from "./page.module.scss";
-import Biography from "@/components/Biography/Biography";
+import Article from "@/components/Article/Article";
 
 export default function ArticlePage({ parameters }: any) {
   return (
     <div className={styles.article_page}> 
       <div className={styles.article_page_wrapper}>
-        <Biography title="Биография Абая" content="Слова"/>
+        <Article title="Биография Абая" content="Слова"/>
       </div>
     </div>
     );
--- components/ArticleCard/ArticleCard.tsx
+++ components/ArticleCard/ArticleCard.tsx
@@ -2,19 +2,13 @@ import styles from "./ArticleCard.module.scss";
 
 export default function ArticleCard({
   topic = "",
-  type = "",
 }: {
   topic?: string;
-  type?: string;
 }) {
-  let cardId:string = "";
-  if(type == "biography"){
-    cardId = `${styles.biography}`;
-  }
   return (
     <>
-      <div className={styles.card} id={cardId}>
-        <div className={styles.card__inner_text} id={cardId}>
+      <div className={styles.card + " glass"}>
+        <div className={styles.card__inner_text} >
           {topic}
         </div>
       </div>
