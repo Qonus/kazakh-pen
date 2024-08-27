6e9102e68bf6d3933158d8cf807ce0a494a33a51
Boiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii <bolathanzada@gmail.com>
2024-08-27 11:17:53 +0500

    [Add] publish date & las change date in article

--- components/ArticleComponent/Article.tsx
+++ components/ArticleComponent/Article.tsx
@@ -2,18 +2,21 @@ import styles from "./Article.module.scss";
 
 export default function Article({
   title = "Заголовок",
+  publish_date,
+  last_changed,
   content = "Содережание",
-  language,
 }: {
   title: string;
+  publish_date:string;
+  last_changed: string;
   content: string;
-  language?: string;
 }) {
   return (
     <div className={styles.article}>
       <div className={styles.article_wrapper}>
         <div className={styles.title}>
           <h1>{title}</h1>
+          <p className={styles.dates}> дата публикации: {publish_date}, последнее изменение:{last_changed} </p>
         </div>
         <p className={styles.p}>{content}</p>
       </div>
